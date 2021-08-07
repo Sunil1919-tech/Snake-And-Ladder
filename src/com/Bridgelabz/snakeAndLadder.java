c4d579a721640ec1e41398a5c91dbd977d82946c
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-07 12:16:33 +0530

    Added: dice rolled and num between 1 to 6

--- src/com/Bridgelabz/snakeAndLadder.java
+++ src/com/Bridgelabz/snakeAndLadder.java
@@ -3,8 +3,13 @@ package com.Bridgelabz;
 public class snakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game.");
-          int startPosition=0;
+
+        //variables
+        int startPosition = 0;
+
         System.out.println("The player postion is " + startPosition);
+        double diceNo = Math.floor(((Math.random() * 10) % 6) + 1); //to get number b/w 1to6
+        System.out.println("The Dice  number is = " + diceNo);
 
     }
 }
