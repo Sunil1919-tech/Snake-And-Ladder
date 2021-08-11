5563d64115c863ded1117d8f61869c2432a2e4a1
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-11 09:55:15 +0530

    Refactor: created snake and Ladder for Two Players

--- src/com/Bridgelabz/snakeAndLadder.java
+++ src/com/Bridgelabz/snakeAndLadder.java
@@ -1,55 +1,111 @@
 package com.Bridgelabz;
+/*
+program for Snake and Ladder
+declering logics for die position, Win position and play for 2
+ */
 
 public class snakeAndLadder {
+    //declering constant variables
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static final int NO_PLAY = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game.");
-
-        //constants
-        int LADDER = 1;
-        int SNAKE = 2;
-        int NO_PLAY = 0;
-
-//variable decleration
-        int startPosition = 0;
+         //variable decleration
+        int player1Position=0;
+        int player2Position=0;
         int newPosition = 0;
-        int winPosition = 100;
-        int count=0;
+        int currentPlayer=1;
+        int count1 = 0;
+        int count2=0;
+        int option;
 
-        while (startPosition < winPosition) {
+        //delcaring while loop condition for player1 & player2 Turns
+        while (true) {
+            //Logic for Player 1
+            if (currentPlayer == 1) {
+                System.out.println("Now Player-1's turn");
+                count1++;
+                //random function to get dice number
+                int diceNum = (int) Math.floor((Math.random() * 6) + 1);
+                System.out.println("The Die number is = " +diceNum);
+                //logic to know whether it snake & ladder & no play
+                option = (int) Math.floor((Math.random() * 3));
+                System.out.println("The Option is =" +option);
 
-            //random function to get dice number
-            int diceNum = (int) Math.floor(((Math.random() * 10) % 6) + 1); //to get number b/w 1to6
-            System.out.println("The Dice  number is = " + diceNum);
-            //logic to know whether it snake & ladder & no play
-            int option = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println("The option is " + option);
-            count++;
-
-            switch (option) {
-                case 1:
-                    option = LADDER;
-                    newPosition += diceNum;
+                switch (option) {
+                    case NO_PLAY:
+                        newPosition = 0;
+                        break;
+                    case LADDER:
+                        newPosition = diceNum;
+                        break;
+                    case SNAKE:
+                        newPosition = -diceNum;
+                        break;
+                }
+                player1Position = player1Position + newPosition;
+                if (player1Position < 0) {
+                    player1Position = 0;
+                }
+                if (player1Position > 100) {
+                    player1Position = newPosition;
+                }
+                System.out.println("now Player-1 at " + player1Position + "th" + " Position");
+                if (player1Position == 100) {
                     break;
-                case 2:
-                    option = SNAKE;
-                    newPosition -= diceNum;
+                }
+            }
+            else {
+                //logic for player 2
+                count2++;
+                System.out.println("Now Player-2's turn");
+                int DieRolled = (int) (Math.random() * 6) + 1;
+                option = (int) (Math.random() * 3);
+
+                switch (option){
+                    case NO_PLAY:
+                        newPosition = 0;
+                        break;
+                    case LADDER:
+                        newPosition = DieRolled;
+                        break;
+                    case SNAKE:
+                        newPosition = -DieRolled;
+                        break;
+                }
+                player2Position = player2Position + newPosition;
+                if (player2Position < 0) {
+                    player2Position = 0;
+                }
+                if (player2Position > 100) {
+                    player2Position = newPosition;
+                }
+                System.out.println("Now Player-2 at " + player2Position + "th" + " Position");
+                if (player2Position == 100) {
                     break;
-                default:
-                    option = NO_PLAY;
-                    newPosition += 0;
+                }
             }
-            startPosition = startPosition + newPosition; //calculating the position of the dice
-
-            if (startPosition < 0) {
-                System.out.println("Restart the game");
-                startPosition = 0;
+            if (option==1) {
+                System.out.println("You got Ladder. Now play again.");
             }
-            if (startPosition > 100) {
-                startPosition = newPosition;
+            else {
+                if (currentPlayer==1) {
+                    currentPlayer = 2;
+                }
+                else  {
+                    currentPlayer = 1;
+                }
             }
-
-            System.out.println("You are now at " + startPosition + "th" + " position");
         }
-        System.out.println("\nCongratulations.. you won! the Game \t after rolling the Die " + count+"position");
+        if (player1Position==100) {
+            System.out.println("\nCongratulation Player-1 you won! \n after die Rolled"+ count1+" times.");
+        } else {
+            System.out.println("\nCongratulation Player-2 you won! \n after die Rolled"+ count2+" times.");
+        }
+
+        }
+
     }
-}
+
